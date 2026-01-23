268c67c92e113eb35b545ea7136e58fb6e8a9fa0
Viky Sangoi <vikysangoi@gofynd.com>
2026-01-23 10:53:41 +0000

    File changes

--- src/components/accounts/AccountForm.jsx
+++ src/components/accounts/AccountForm.jsx
@@ -6,6 +6,7 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Switch } from "@/components/ui/switch";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import CurrencySelect from "../shared/CurrencySelect";
 
 const accountTypes = [
